be49222da34f5bd4621a6bd24fd20768b45a2058
pth9719 <35356463+pth9719@users.noreply.github.com>
2022-09-14 16:20:44 +0900

    Add files via upload

---
+++ java/hk220914/src/hk220914/TestClass.java
@@ -0,0 +1,8 @@
+package hk220914;
+
+public class TestClass {
+	public static void main(String [] args)	{
+		System.out.println("hello");
+	}
+}
+
---
+++ java/hk220914/src/hk220914/TestClass3.java
@@ -0,0 +1,7 @@
+package hk220914;
+
+public class TestClass3 {
+	public static void main(String[] args)	{
+		System.out.println("Hello, Java");
+	}
+}
